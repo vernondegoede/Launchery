36852ed6a241bb0b3dd28c77d4a82551a4d8cecf
Vernon de Goede <vernon@mollie.com>
2018-08-17 09:21:35 +0200

    Added caption-core and mollie-api-node

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,15 @@ const projects = [
     githubRepo: "https://github.com/gielcobben/caption",
   },
   {
+    title: "Caption Core",
+    repo: "caption-core",
+    downloads: "0",
+    icon: "http://vernondegoede.com/static/open-source/caption-core.png",
+    type: "npm",
+    githubRepo: "https://github.com/gielcobben/caption-core",
+    downloadUrl: "https://getcaption.co",
+  },
+  {
     title: "verminal",
     repo: "verminal",
     downloads: "11,000",
@@ -21,6 +30,17 @@ const projects = [
     countType: "monthly",
     githubRepo: "https://github.com/defringe/verminal",
     downloadUrl: "https://hyper.is/plugins/verminal",
+  },
+  {
+    title: "mollie-api-node",
+    repo: "mollie-api-node",
+    downloads: "0",
+    icon:
+      "https://images.ctfassets.net/k4kk06v59kf0/3Y71BBif6UcOOkeKguOagk/4bb3f507e602719a476e40d4b2f17583/icon-node.svg",
+    type: "npm",
+    countType: "monthly",
+    githubRepo: "https://github.com/mollie/mollie-api-node",
+    downloadUrl: "https://www.npmjs.com/package/mollie-api-node",
   },
 ];
 
